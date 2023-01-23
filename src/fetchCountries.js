538648bcf4ca4c070eefd413553e41683b87825f
BoroDinS <sergey.borodin.lg@gmail.com>
2023-01-23 22:38:31 +0100

    make conditional rendering, filter response, render markup

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,8 @@
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(
+
+  const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+
+  return fetch(url).then(
     (response) => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -8,4 +11,3 @@ export function fetchCountries(name) {
     }
   );
 }
-
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchCountries } from './css/fetchCountries';
+import { fetchCountries } from './fetchCountries';
 
 import './css/styles.css';
 
@@ -6,6 +6,7 @@ const DEBOUNCE_DELAY = 300;
 
 const countryNameInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 countryNameInput.addEventListener('input', OnInputChange);
 
@@ -15,14 +16,28 @@ function OnInputChange(e) {
 }
 
 function renderCountryList(countries) {
-  const markup = countries
-    .map((country) => {
-      return `<li>
-          <p><b>Name</b>: ${country.name.official}</p>
-          <p><b>Capital</b>: ${country.capital}</p>
-          <p><b>Population</b>: ${country.population}</p>
-        </li>`;
-    })
-    .join("");
-  countryList.innerHTML = markup;
+    console.log(countries);
+
+    // Markup rendering in a case when backend sends array of countries
+    if (countries.length === 1) {
+        const [country] = countries;
+  const {name, flags, capital, population, languages} = country;
+            const listMarkup = `<p><img src=${flags.png} alt="Flag of ${name}" width='20'> ${name}</p>
+        <p><b>Capital</b>: ${capital}</p>
+        <p><b>Population</b>: ${population}</p>
+        <p><b>Languages</b>: ${languages.map(language => `${language.name}`).join(",")}</p>`;
+        countryList.innerHTML = '';
+      countryInfo.innerHTML = listMarkup;
 }
+    // Markup rendering in a case when backend sends array with one single country
+    else if (countries.length > 1) {
+            const detailMarkup = countries.map(({name, flags}) =>
+            `<li class='countryListItem'><img class='flagImage' src=${flags.png} alt="Flag of ${name}" width='20'><p>${name}</p></li>`
+        )
+        .join("");
+        
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = detailMarkup;
+        }
+}
+  
